414a374c3dc29044794d77af8550e9f85a0969d5
emperor-maroond <erasmus.devlon@gmail.com>
2021-04-26 13:26:07 +0200

    started inserting contact orders

--- droid/src/bot_description/scripts/code.py
+++ droid/src/bot_description/scripts/code.py
@@ -3,11 +3,18 @@ import rospy as rp
 
 from std_msgs.msg import Float64
 
-pub = rp.Publisher("")
+pub = [4]
+
+pub[0] = rp.Publisher("/bot/RevR_position_controller/command", Float64, queue_size=10)
+pub[1] = rp.Publisher("/bot/RevL_position_controller/command", Float64, queue_size=10)
+pub[2] = rp.Publisher("/bot/SliderR_position_controller/command", Float64, queue_size=10)
+pub[3] = rp.Publisher("/bot/SliderL_position_controller/command", Float64, queue_size=10)
 
 data = np.load('data.npy')
 
 print(data)
 
 if __name__ == '__main__': 
+    for n in range(100):
+        pass    
     pass
