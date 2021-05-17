31f0bf1321c0d9c09800250f849d518cd7aace48
emperor-maroond <erasmus.devlon@gmail.com>
2021-05-17 12:08:41 +0200

    busy with gazebo

--- droid/src/bot_description/scripts/code.py
+++ droid/src/bot_description/scripts/code.py
@@ -2,10 +2,15 @@
 
 import numpy as np
 import rospy as rp
+import cloudpickle
+import time
 
 from std_msgs.msg import Float64
+from std_srvs.srv import Empty
 
 rp.init_node('commander')
+
+reset_simulation = rp.ServiceProxy('/gazebo/reset_simulation', Empty)
 
 pub = [None]*4
 
@@ -14,42 +19,35 @@ pub[1] = rp.Publisher("/bot/RevL_position_controller/command", Float64, queue_si
 pub[2] = rp.Publisher("/bot/SliderR_position_controller/command", Float64, queue_size=10)
 pub[3] = rp.Publisher("/bot/SliderL_position_controller/command", Float64, queue_size=10)
 
-data = np.load('data.npy', allow_pickle=True)
-#print(data[1][0])
+data = [None]*3
 
-'''data = asarray([[N],                                                         0
-                [m.tau_a_L[n].value for n in range (1, N+1)],                   1
-                [m.Fbang_pos_L[n].value for n in range (1, N+1)],               2
-                [m.Fbang_neg_L[n].value for n in range (1, N+1)],               3
-                [m.FhardStop_ext_L[n].value for n in range (1, N+1)],           4
-                [m.FhardStop_rtn_L[n].value for n in range (1, N+1)],           5
-                [m.q0[n,'theta_l_L'].value for n in range (1, N+1)],            6   
-                [m.q0[n,'r_L'].value for n in range (1, N+1)],                  7
-                [m.tau_a_R[n].value for n in range (1, N+1)],                   8
-                [m.Fbang_pos_R[n].value for n in range (1, N+1)],               9
-                [m.Fbang_neg_R[n].value for n in range (1, N+1)],               10
-                [m.FhardStop_ext_R[n].value for n in range (1, N+1)],           11
-                [m.FhardStop_rtn_R[n].value for n in range (1, N+1)],           12     
-                [m.q0[n,'theta_l_R'].value for n in range (1, N+1)],            13
-                [m.q0[n,'r_R'].value for n in range (1, N+1)],                  14
-                [m.q0[n,'x'].value for n in range (1, N+1)],                    15
-                [m.q0[n,'z'].value for n in range (1, N+1)],                    16
-                [m.damping],                                                    17
-                [m.F_max]])                                                     18'''
+with open("accel.pkl", "rb") as f:
+    data[0] = cloudpickle.load(f)
 
-N = data[0][0]
+with open("steady-state.pkl", "rb") as f:
+    data[1] = cloudpickle.load(f)      
 
+with open("decel.pkl", "rb") as f:
+    data[2] = cloudpickle.load(f)
+
+N = data[1].N[-1]
 if __name__ == '__main__': 
-    for n in range(1, 5):
-        pub[0].publish(0.0)
-        pub[1].publish(0.0)
-        pub[2].publish(0.0)
-        pub[3].publish(0.0)
-        rp.sleep(0.1)
+    pub[0].publish(data[0].tau_a_R[1].value)
+    pub[1].publish(data[0].tau_a_L[1].value)
+    pub[2].publish(-data[0].F_max)
+    pub[3].publish(-data[0].F_max)
 
-    for n in range(1, N):
-        pub[0].publish(data[8][n])
-        pub[1].publish(data[1][n])
-        pub[2].publish(data[18][0]*(data[9][n] - data[10][n]))
-        pub[3].publish(data[18][0]*(data[2][n] - data[3][n]))
-        rp.sleep(0.1)
+    reset_simulation()
+    time.sleep(1)
+
+    for i in range(0, len(data)):
+        for n in range(1, N+1):
+            pub[0].publish(data[i].tau_a_R[n].value)
+            pub[1].publish(data[i].tau_a_L[n].value)
+            Fnet = data[i].F_max*(data[i].Fbang_pos_R[n].value - data[i].Fbang_neg_R[n].value)\
+                    - data[i].dq0[n,'r_R'].value*data[i].damping - data[i].FhardStop_ext_R[n].value + data[i].FhardStop_rtn_R[n].value
+            pub[2].publish(Fnet)
+            Fnet = data[i].F_max*(data[i].Fbang_pos_L[n].value - data[i].Fbang_neg_L[n].value)\
+                    - data[i].dq0[n,'r_L'].value*data[i].damping - data[i].FhardStop_ext_L[n].value + data[i].FhardStop_rtn_L[n].value
+            pub[3].publish(Fnet)
+            #rp.sleep(0.1)
