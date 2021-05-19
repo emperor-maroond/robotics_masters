96c87cc9ef158d633adff79f1123ed86e1a3cc41
emperor-maroond <erasmus.devlon@gmail.com>
2021-05-19 14:38:24 +0200

    got Gazebo sorted

--- droid/src/bot_description/scripts/code.py
+++ droid/src/bot_description/scripts/code.py
@@ -21,13 +21,13 @@ pub[3] = rp.Publisher("/bot/SliderL_position_controller/command", Float64, queue
 
 data = [None]*3
 
-with open("accel.pkl", "rb") as f:
+with open("accel.bin", "rb") as f:
     data[0] = cloudpickle.load(f)
 
-with open("steady-state.pkl", "rb") as f:
+with open("steady-state.bin", "rb") as f:
     data[1] = cloudpickle.load(f)      
 
-with open("decel.pkl", "rb") as f:
+with open("decel.bin", "rb") as f:
     data[2] = cloudpickle.load(f)
 
 N = data[1].N[-1]
@@ -39,18 +39,18 @@ if __name__ == '__main__':
     pub[2].publish(-data[0].F_max)
     pub[3].publish(-data[0].F_max)
 
-    time.sleep(1)
+    time.sleep(2)
 
     for i in range(0, len(data)):
         for n in range(1, N+1):
             pub[0].publish(data[i].q0[n, 'theta_l_R'].value)
             pub[1].publish(data[i].q0[n, 'theta_l_L'].value)
-            Fnet = data[i].F_max*(data[i].Fbang_pos_R[n].value - data[i].Fbang_neg_R[n].value) - data[i].dq0[n,'r_R'].value*data[i].damping - data[i].FhardStop_ext_R[n].value + data[i].FhardStop_rtn_R[n].value
-            if(Fnet>=data[0].F_max or Fnet<=-data[0].F_max):
-                pub[2].publish(Fnet)
-            Fnet = data[i].F_max*(data[i].Fbang_pos_L[n].value - data[i].Fbang_neg_L[n].value) - data[i].dq0[n,'r_L'].value*data[i].damping - data[i].FhardStop_ext_L[n].value + data[i].FhardStop_rtn_L[n].value
-            if(Fnet>=data[0].F_max or Fnet<=-data[0].F_max):
-                pub[3].publish(Fnet)
+            F = data[i].F_max*(data[i].Fbang_pos_R[n].value - data[i].Fbang_neg_R[n].value) #- data[i].dq0[n,'r_R'].value*data[i].damping - data[i].FhardStop_ext_R[n].value + data[i].FhardStop_rtn_R[n].value
+            # ==if(F>=data[0].F_max or F<=-data[0].F_max):
+            pub[2].publish(F)
+            F = data[i].F_max*(data[i].Fbang_pos_L[n].value - data[i].Fbang_neg_L[n].value) #- data[i].dq0[n,'r_L'].value*data[i].damping - data[i].FhardStop_ext_L[n].value + data[i].FhardStop_rtn_L[n].value
+            # if(F>=data[0].F_max or F<=-data[0].F_max):
+            pub[3].publish(F)
             
             sleep = data[i].h[n].value
-            time.sleep(sleep)
+            rp.sleep(sleep)
