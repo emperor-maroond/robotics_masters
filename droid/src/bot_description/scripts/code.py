f471325c7d8cf322d7f6b64fcc2296a94723c705
emperor-maroond <erasmus.devlon@gmail.com>
2021-06-23 09:35:20 +0200

    updated model as well as made some changes to the code to adjust for refrence points

--- droid/src/bot_description/scripts/code.py
+++ droid/src/bot_description/scripts/code.py
@@ -34,8 +34,8 @@ N = data[1].N[-1]
 if __name__ == '__main__': 
     reset_simulation()
 
-    pub[0].publish(data[0].q0[1, 'theta_l_R'].value)
-    pub[1].publish(data[0].q0[1, 'theta_l_L'].value)
+    pub[0].publish(data[0].q0[1, 'theta_l_R'].value - np.pi/2)
+    pub[1].publish(data[0].q0[1, 'theta_l_L'].value - np.pi/2)
     pub[2].publish(-data[0].F_max)
     pub[3].publish(-data[0].F_max)
 
@@ -43,13 +43,11 @@ if __name__ == '__main__':
 
     for i in range(0, len(data)):
         for n in range(1, N+1):
-            pub[0].publish(data[i].q0[n, 'theta_l_R'].value)
-            pub[1].publish(data[i].q0[n, 'theta_l_L'].value)
-            F = data[i].F_max*(data[i].Fbang_pos_R[n].value - data[i].Fbang_neg_R[n].value) #- data[i].dq0[n,'r_R'].value*data[i].damping - data[i].FhardStop_ext_R[n].value + data[i].FhardStop_rtn_R[n].value
-            # ==if(F>=data[0].F_max or F<=-data[0].F_max):
+            pub[0].publish(data[i].q0[n, 'theta_l_R'].value - np.pi/2)
+            pub[1].publish(data[i].q0[n, 'theta_l_L'].value - np.pi/2)
+            F = data[i].F_max*(data[i].Fbang_pos_R[n].value - data[i].Fbang_neg_R[n].value)
             pub[2].publish(F)
-            F = data[i].F_max*(data[i].Fbang_pos_L[n].value - data[i].Fbang_neg_L[n].value) #- data[i].dq0[n,'r_L'].value*data[i].damping - data[i].FhardStop_ext_L[n].value + data[i].FhardStop_rtn_L[n].value
-            # if(F>=data[0].F_max or F<=-data[0].F_max):
+            F = data[i].F_max*(data[i].Fbang_pos_L[n].value - data[i].Fbang_neg_L[n].value)
             pub[3].publish(F)
             
             sleep = data[i].h[n].value
