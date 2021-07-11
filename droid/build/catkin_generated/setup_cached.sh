7b6ca596f0a5c7a49fcb1ee026c691cceb23f8f2
emperor-maroond <erasmus.devlon@gmail.com>
2021-07-11 16:00:05 +0200

    some init msg

--- droid/build/catkin_generated/setup_cached.sh
+++ droid/build/catkin_generated/setup_cached.sh
@@ -7,5 +7,4 @@
 # new environment variables
 
 # modified environment variables
-export PATH='/home/devlon/catkin_ws/devel/bin:/opt/ros/noetic/bin:/home/devlon/anaconda3/bin:/home/devlon/anaconda3/condabin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/mnt/c/Program Files (x86)/Common Files/Oracle/Java/javapath:/mnt/c/ProgramData/Oracle/Java/javapath:/mnt/c/WINDOWS/system32:/mnt/c/WINDOWS:/mnt/c/WINDOWS/System32/Wbem:/mnt/c/WINDOWS/System32/WindowsPowerShell/v1.0:/mnt/c/WINDOWS/System32/OpenSSH:/mnt/c/Program Files/dotnet:/mnt/c/Users/erasm/AppData/Local/Microsoft/WindowsApps:/mnt/c/Users/erasm/AppData/Local/Programs/Microsoft VS Code/bin:/snap/bin'
 export PWD='/home/devlon/robotics_masters/droid/build'
