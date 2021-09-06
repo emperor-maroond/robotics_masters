f6dc40b3aea66678e71491f525affe9d4be8201b
emperor-maroond <erasmus.devlon@gmail.com>
2021-09-06 12:55:27 +0200

    added lines to dump sensor data to a txt file on close

--- droid/src/bot_description/scripts/ros_serial.py
+++ droid/src/bot_description/scripts/ros_serial.py
@@ -5,16 +5,22 @@ import rospy
 
 from my_message.msg import my_message
 
+# Some arrays to store the data that will be dumped in a text file
+tmp1 = []
+tmp2 = []
+tmp3 = []
+tmp4 = []
+
 def callback(data):
     servoFeed_R = data.some_floats[0]
     servoFeed_L = data.some_floats[1]
     encoder_1 = data.some_floats[2]
     encoder_2 = data.some_floats[3]
 
-    print(servoFeed_R)
-    print(servoFeed_L)
-    print(encoder_1)
-    print(encoder_2)
+    tmp1.append(servoFeed_R)
+    tmp2.append(servoFeed_L)
+    tmp3.append(encoder_1)
+    tmp4.append(encoder_2)
 
 def listener():
     try:
@@ -24,6 +30,12 @@ def listener():
         while not rospy.core.is_shutdown():
             rospy.rostime.wallsleep(0.5)
     except KeyboardInterrupt:
+        file = open('data.txt', 'w')
+        file.write('Servo Feedback Right:\n', tmp1)
+        file.write('Servo Feedback Left:\n', tmp2)
+        file.write('Encoder data 1:\n', tmp3)
+        file.write('Encoder data 2:\n', tmp4)
+        file.close()
         print('Adios!')
 
 if __name__ == '__main__':
