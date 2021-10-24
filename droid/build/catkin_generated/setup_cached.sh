32006499d90d45e53565be5296bdf140d560192a
emperor-maroond <erasmus.devlon@gmail.com>
2021-10-24 11:37:41 +0200

    new

--- droid/build/catkin_generated/setup_cached.sh
+++ droid/build/catkin_generated/setup_cached.sh
@@ -7,7 +7,8 @@
 # new environment variables
 
 # modified environment variables
-export CMAKE_PREFIX_PATH="/home/callen/Documents/robotics_masters/droid/devel:$CMAKE_PREFIX_PATH"
-export PWD='/home/callen/Documents/robotics_masters/droid/build'
-export ROSLISP_PACKAGE_DIRECTORIES='/home/callen/Documents/robotics_masters/droid/devel/share/common-lisp'
-export ROS_PACKAGE_PATH="/home/callen/Documents/robotics_masters/droid/src:$ROS_PACKAGE_PATH"
+export CMAKE_PREFIX_PATH="/home/devlon/robotics_masters/droid/devel:$CMAKE_PREFIX_PATH"
+export PATH='/opt/ros/noetic/bin:/home/devlon/anaconda3/bin:/home/devlon/anaconda3/condabin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin'
+export PWD='/home/devlon/robotics_masters/droid/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/devlon/robotics_masters/droid/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/devlon/robotics_masters/droid/src:$ROS_PACKAGE_PATH"
