473b9c63a43d470309a73a98b19c21fa26fa6e23
emperor-maroond <erasmus.devlon@gmail.com>
2022-03-17 15:11:00 +0200

    updated open loop ctrl

--- droid/src/bot_description/scripts/ros_code.py
+++ droid/src/bot_description/scripts/ros_code.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env python3
 
 import numpy as np
-import rospy
+import rospy as rp
 import cloudpickle
 
 from my_message.msg import my_message
 
 # Set-up ROS and read in trajectory data_________________________________________________________
 
-rospy.init_node('master')
+rp.init_node('master')
 
 pub_time = 10/1000
-# rate = rospy.Rate(1/pub_time)
-rate = rospy.Rate(1000)
+# rate = rp.Rate(1/pub_time)
+rate = rp.Rate(1/pub_time)
 
-pub = rospy.Publisher('chatter', my_message, queue_size=10)
+pub = rp.Publisher('chatter', my_message, queue_size=10)
   
 message = my_message()
 message.some_floats = []
@@ -32,6 +32,12 @@ with open("Feasible_Solution/short1/decel.pkl", "rb") as f:
 
 
 # Code__________________________________________________________________________________________
+def d2r(deg):
+    return deg*np.pi/180
+
+def r2d(rad):
+    return rad*180/np.pi
+
 
 # Var for data that will be interpolated
 F_max = data[0].F_max
@@ -54,6 +60,13 @@ N_time = []
 cN_time = []
 N_adder = 0
 cN_adder = 0
+
+# For holding message data
+dat = [None]*4
+dat[0] = 0 # Rev Right
+dat[1] = 0 # Rev Left
+dat[2] = 0 # Slider Right
+dat[3] = 0 # Slider Left
 
 def accel():
     N = data[0].N[-1]
@@ -115,18 +128,65 @@ def interpolate(y, cur_time, x, position):
 
 def send_message():
     global message
-    rospy.loginfo('send data')
-    for n in range(0, len(servo_R)):
-        message.some_floats.append(servo_R[n])
-        message.some_floats.append(servo_L[n])
-        message.some_floats.append(solenoid_R[n])
-        message.some_floats.append(solenoid_L[n])
+    # rp.loginfo('send data:')
+    for n in range(0, len(dat)):
+        message.some_floats.append(dat[n])
+    
+    # rp.loginfo(message)
+    pub.publish(message)
+    message.some_floats.clear()
 
-        rospy.loginfo(message)
-        pub.publish(message)
-        message.some_floats.clear()
 
-        rate.sleep()
+# Callback______________________________________________________________________________
+ser_R = []
+ser_L = []
+enc_1 = []
+enc_2 = []
+
+i = 0
+
+def callback(data):
+    global ser_R, ser_L, enc_1, enc_2
+    servoFeed_R = data.some_floats[0] 
+    servoFeed_L = data.some_floats[1]
+    encoder_1 = data.some_floats[2] # Height
+    encoder_2 = data.some_floats[3] # Length travled
+    rad = d2r(encoder_1)
+
+    ser_R.append(servoFeed_R)
+    ser_L.append(servoFeed_L)
+    enc_1.append(encoder_1)
+    enc_2.append(encoder_2)
+
+    if round(servoFeed_R,5) == round(servo_R[i],5) and round(servoFeed_L,5) == round(servo_L[i],5):
+        i+=1
+        dat[0] = servo_R[i]
+        dat[1] = servo_L[i]
+        dat[2] = solenoid_R[i]
+        dat[3] = solenoid_L[i]
+        
+        if i >= len(servo_R):
+            i = len(servo_R) - 1
+        
+    send_message()
+
+def listener():
+    try:
+        rp.Subscriber('sensor_data', my_message, callback)
+        while not rp.core.is_shutdown():
+            rp.rostime.wallsleep(0.5)
+    except KeyboardInterrupt:
+        # destroy()
+        # rp.signal_shutdown("Adios")
+        print('Bye')
+        
+        file = open('/home/devlon/robotics_masters/data.txt', 'a')
+        file.write('Servo Feedback Right:\n {}\n'.format(ser_R))
+        file.write('Servo Feedback Left:\n {}\n'.format(ser_L))
+        file.write('Encoder data 1:\n {}\n'.format(enc_1))
+        file.write('Encoder data 2:\n {}\n'.format(enc_2))
+        file.close()
+        # print('Adios!')
 
 if __name__ == '__main__':
     accel()
@@ -139,9 +199,9 @@ if __name__ == '__main__':
             break
 
         tmp, position_cN = interpolate(theta_R, run_time, cN_time, position_cN)
-        servo_R.append(tmp)
+        servo_R.append(r2d(tmp))
         tmp, position_cN = interpolate(theta_L, run_time, cN_time, position_cN)
-        servo_L.append(tmp)
+        servo_L.append(r2d(tmp)) 
 
         tmp, position_N = interpolate(F_bang_R, run_time, N_time, position_N)
         if tmp >= 0.13:
@@ -161,6 +221,10 @@ if __name__ == '__main__':
         run_time += pub_time
 
     try:
-        send_message()
-    except rospy.ROSInterruptException:
+        dat[0] = servo_R[i]
+        dat[1] = servo_L[i]
+        dat[2] = solenoid_R[i]
+        dat[3] = solenoid_L[i]
+        listener()
+    except rp.ROSInterruptException:
         pass
